347fe5e0f8a0d4163da442b10e068e0facb64f86
Federico Iachetti <iachetti.federico@gmail.com>
2015-05-20 23:02:24 -0300

    Added registry

--- lucid_dream.rb
+++ lucid_dream.rb
@@ -6,8 +6,22 @@ require "fattr"
 require "string_plus"
 
 module LucidDream
+  class InMemoryRegistry
+    def journal_repository
+      journals = [
+                  Journal.new(name: "My first journal"),
+                  Journal.new(name: "two"),
+                  Journal.new(name: "three"),
+                  Journal.new(name: "four"),
+                 ]
+      InMemoryJournalRepository.new(journals)
+    end
 
     
+  end
+  
+  def self.registry
+    @registry ||= InMemoryRegistry.new
   end
   
   class WebApp < Roda
@@ -18,35 +32,49 @@ module LucidDream
     plugin :partials
     plugin :view_options
 
-  require "./assets/assets"
-  
-  Dir["./models/*.rb"].each{|f| require f}
-  Dir["./repositories/*.rb"].each{|f| require f}
+    plugin :json
+    plugin :path_matchers
+    plugin :empty_root
 
-  plugin :extension_matcher
+    require "./assets/assets"
 
-  plugin :haml_helpers
-  plugin :foundation_helpers
+    Dir["./models/*.rb"].each{|f| require f}
+    Dir["./repositories/*.rb"].each{|f| require f}
 
-  route do |r|
-    r.assets
-    r.root do
-      view "welcome"
-    end
+    plugin :extension_matcher
 
-    r.on "journal" do
-      # Authenticate
+    plugin :haml_helpers
+    plugin :foundation_helpers
+    
+    plugin :mystique
+    Dir["./presenters/*.rb"].each{|f| require f}
+    
+    route do |r|
+      r.assets
       r.root do
-        view "journal/journal"
+        view "welcome"
       end
-    end
 
-    r.extension("json") do
-      r.on "api" do
-        r.is "welcome" do
-          {
-           from: "JSON API",
-          }
+      r.on "journal" do
+        journal_repo = LucidDream.registry.journal_repository
+        r.root do
+          view "journal/journals", locals: {journals: journal_repo.all}
+        end
+
+        r.on ":slug" do |slug|
+          r.root do
+            slug
+          end
+        end
+      end
+
+      r.extension("json") do
+        r.on "api" do
+          r.is "welcome" do
+            {
+             from: "JSON API",
+            }
+          end
         end
       end
     end
