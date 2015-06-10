0717aee3c04751e13ecfbb6bc1a315985857dfc7
Federico Iachetti <iachetti.federico@gmail.com>
2015-06-10 09:20:58 -0300

    Chaged Foundation for Material

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ group :development do
   gem "pry", "~> 0.10.1"
   gem "pessimize", "~> 0.1.0"
   gem "rake", "~> 10.4.2"
+  gem "guard", "~> 2.12.5"
+  gem "guard-rack", "~> 2.1.1"
+  gem "guard-livereload", "~> 2.4.0"
 end
 
 gem "thin", "~> 1.6.3"
@@ -17,3 +20,4 @@ gem "dotenv", "~> 2.0.1"
 gem "fattr", "~> 2.2.2"
 gem "mystique", "~> 0.5.1"
 gem "string_plus", "~> 0.4.1"
+gem "sass", "~> 3.4.13"
--- assets/assets.rb
+++ assets/assets.rb
@@ -1,20 +1,18 @@
 class LucidDream::WebApp < Roda
   def self.css_files
-    %w[normalize.css foundation.min.css]
-  end
-
-  def self.foundation_plugins(*plugins)
-    plugins.map{|f| "foundation/foundation.#{f}.js" }
+    %w[materialize.scss]
   end
 
   def self.js_files
     js = []
-    js.concat %w[vendor/jquery.js vendor/modernizr.js vendor/fastclick.js foundation/foundation.js]
-    js.concat foundation_plugins("alert", "topbar")
+    js.concat %w[vendor/jquery.js]
+    js << "materialize.min.js"
     js << "main.js"
     js
   end
 
-  plugin :assets, css: css_files, js: js_files
+  dependencies = Hash.new(Dir["assets/" "**/*.*"])
+
+  self.plugin :assets, css: css_files, js: js_files, dependencies: dependencies
   compile_assets if production?
 end
